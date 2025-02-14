05216c6a1687c2a7a12226b4982baae82a2f080e
Bayu Adji Prakoso <bayuadjiprakoso@gmail,com>
2025-02-14 17:41:05 +0700

    selesai melakukan latihan php

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php 
+    $nama = "Bayu Adji Prakoso";
+    $email = "bayuadjiprakoso@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama<h1>";
+    echo "<h4>$email<h4>";
+    echo "<p>Halo,Nama saya $nama, Saya sebagai $pekerjaan Di Prodi $jurusan $universitas<p>";
+?>
---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,7 @@
+<?php
+$part1 = "Topi";
+$part2 = "Bundar";
+
+
+echo "<h2><i>\" " . $part1 . " saya " . $part2 . ", " . $part2 . " " . $part1 . " saya.\"</i></h2>";
+?>
