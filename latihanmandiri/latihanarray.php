520535ca2f674b22c9b057f3c49f323aab0ef068
Bayu Adji Prakoso <bayuadjiprakoso@gmail,com>
2025-03-13 15:09:33 +0700

    latihan mandiri

---
+++ latihanmandiri/latihanarray.php
@@ -0,0 +1,28 @@
+<?php 
+// array
+// varable yg memiliki banyak nilai
+//elemen pada array boleh memiliki tipe data yang berbeda
+//pasangan antara key dan value
+//key adaalah index yang di mulai dari 0
+
+//membuat array
+//cara lama
+$hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
+//cara baru
+$bulan = ["Januari", "Februari", "Maret", "April"];
+$arr1 = [123, "what", false];
+
+//menampilkan array
+//var_dump() / print_r()
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+
+
+
+
+
+
+
+
+?>
